aac1e4399cae74193909047dd42716a26cafb6c4
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-04 18:50:46 +0530

    fixes

--- src/app/Components/ConnectCard/index.tsx
+++ src/app/Components/ConnectCard/index.tsx
@@ -1,6 +1,8 @@
 import { connectSvg } from '@/assets/avatar'
+import { InstagramIcon } from '@/assets/icons'
 import React from 'react'
 import Circle from "../AnimatedCircles/index"
+import { socialList } from './socialItems'
 import "./style.scss"
 
 const ConnectCard = () => {
@@ -12,7 +14,13 @@ const ConnectCard = () => {
             <div className='connect-info'>
             <div className='Prfile-img-mb'>{connectSvg()}</div>
                 <h3>I`m always up for a chat.</h3>
-                <p><span><a href='mailto:Karandeepsinghworkspace@gmail.com'><b>Pop me an email</b></a></span> at @Karandeepsinghworkspace@gmail.com or give me a shout on social media.</p>
+                <p><span><a href='mailto:Karandeepsinghworkspace@gmail.com'><b>Pop me an email</b></a></span> at @Karandeepsinghworkspace@gmail.com or give me a shout on social media.
+                <div>
+                {socialList?.map((social,i) => {
+                return <a key={i} target='_blank' href={social.link}>{social.icon()}</a>
+                })}
+                </div>
+                </p>
             </div>
             <div className='connect-circle'><Circle /></div>
         </div>
---
+++ src/app/Components/ConnectCard/socialItems.ts
@@ -0,0 +1,12 @@
+import { InstagramIcon, LinkedInIcon } from "@/assets/icons";
+
+export const socialList = [
+    {
+        icon : InstagramIcon,
+        link : ''
+    },
+    {
+        icon : LinkedInIcon,
+        link : ''
+    }
+]
