e80171b84aab36df38a1ce1e1d637cb9fe9a1eaf
karandeepsingh3030 <karandeep@nexgeniots.com>
2024-07-04 16:40:25 +0530

    handling resume

--- src/app/Components/About/index.tsx
+++ src/app/Components/About/index.tsx
@@ -6,6 +6,7 @@ import "./style.scss"
 import StudioIcon from '../StudioIcon/index';
 import Arrow from "../../../assets/arrow.png"
 import ShineStudio from "../../../assets/shineStudio.png"
+import resumeFile from "../../../assets/arrow.png"
 
 const About = () => {
     // const handleFadeInAnimation = () => {
@@ -20,13 +21,16 @@ const About = () => {
             <div className='about-content'>
                 <div className='name-text title-font-family abt-name-txt'>Karandeep Singh</div>
                 <div className='about-description'>A <span className='highlight-text-color'>Frontend Engineer </span> with over 4 years of experience in building web applications. My journey into the world of software started with a simple curiosity about how websites work, and it quickly turned into a passion that’s been driving my career ever since.
-                <br></br>Take a look around my portfolio to see some of the exciting projects I`ve worked on.</div>
+                    <br></br>Take a look around my portfolio to see some of the exciting projects I`ve worked on.</div>
                 <div className='resume-btn subtitle-font-size disable-bg-color'>
                     <Image
                         src={Download}
                         alt="Logo"
                     />
-                    Resume</div>
+                    <a href='/Karandeep-Singh-Resume.pdf'
+                        target="_blank"
+                        style={{textDecoration: "none", color: '#000'}}
+                        >Resume</a></div>
             </div>
             <div className='profile-wrapper'>{professionalAvatar()}</div>
         </div>
